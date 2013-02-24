107f508dc62ce72e4a073231b60fe7c85e5370c4
Joshua Underwood <joshuaunderwood7@gmail.com>
2013-02-24 01:59:10 -0700

    fixed greeting

--- pa2functions.cpp
+++ pa2functions.cpp
@@ -11,12 +11,28 @@ bool underwood::DISPLAY_ROW(const T& a, const T& b, const T& c) {...}
 bool underwood::GREETING(void)
 {
     std::cout << "Greetings user." << std::endl
-              << "This program was made for CSCI 3320 Program Assignment 1 Spring 2013:"
+              << "This program was made for CSCI 3320 Program Assignment 2 Spring 2013:"
               << "By: Joshua Underwood"
               << std::endl << std::endl 
-              << "This program is made to use templates" << std::endl    
-              << "It calulates zeros of non-linear functions using the Newton and Secant" << std::endl
-              << "Methods.  These methods are implemented with templates." << std::endl;
+              << "Class requirements" << std::endl
+<< "Write a general template and a specialization for a matrix class. Your matrix classes must" << std::endl
+<< "be based on dynamic arrays that are allocated using operator new. In addition, your" << std::endl
+<< "classes must include the following members." << std::endl
+<< "1. Constant members to return the matrix dimensions." << std::endl
+<< "2. Overloaded operator [ ] to return a matrix cell. Notice that the syntax for returning a" << std::endl
+<< "cell is m[i][j], where m is a matrix, and i and j are the row-column indices" << std::endl
+<< "respectively." << std::endl
+<< "3. T** data member that represents a pointer to a dynamically allocated matrix." << std::endl
+<< "4. Overloaded operator << to display a matrix in a formatted tabular form." << std::endl
+<< "5. The specialization will be for the type int." << std::endl
+<< "Program requirements" << std::endl
+<< "1. Present the user with a greeting." << std::endl
+<< "2. Read the data (V and E) from a file passed to the program in argv[1]." << std::endl
+<< "3. Instantiate the matrix class with the data." << std::endl
+<< "4. Display the matrix." << std::endl
+<< "5. Prompt for and get vertices u and v." << std::endl
+<< "6. Compute a shortest path p between u and v" << std::endl
+<< "7. Display the value of the shortest path that your program computed." << std::endl;
     return true;
 }
 
